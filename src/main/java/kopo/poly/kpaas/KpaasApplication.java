6439afb7210709e7e3b748636b6ab50f75c7e610
김예가 <kimyega@naver.com>
2025-10-28 10:44:12 +0900

    Merge pull request #63
    * hi
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega
    
    * hi
    
    * Merge branch 'dev' into kimyega
    
    * hi
    
    * hi
    
    * Merge remote-tracking branch 'origin/dev' into kimyega
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega
    
    * hi
    
    * hio
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega
    
    * hio
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega
    
    * hio
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega
    
    * hio
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega
    
    * hio
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega
    
    * hio
    
    * hio
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega
    
    * hio
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega
    
    * hio
    
    * hio
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega
    
    * hio
    
    * hio
    
    * hio
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega
    
    * Merge branch 'kimyega' of https://github.com/kimyega/kpaas into kimyega
    
    * hi
    
    * Merge branch 'kimyega' of https://github.com/kimyega/kpaas into kimyega
    
    * hi
    
    * hio
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega
    
    * hio
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega
    
    * hio
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega
    
    * hio
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega
    
    * hio
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega
    
    * hio
    
    * Merge branch 'dev' of https://github.com/kimyega/kpaas into kimyega

--- src/main/java/kopo/poly/kpaas/KpaasApplication.java
+++ src/main/java/kopo/poly/kpaas/KpaasApplication.java
@@ -3,7 +3,10 @@ package kopo.poly.kpaas;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+
+@EnableRedisHttpSession
 @SpringBootApplication
 @MapperScan("kopo.poly.kpaas.mapper")   // ⬅️ 매퍼 패키지 스캔
 public class KpaasApplication {
